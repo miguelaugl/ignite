c77938ccd7f669deec168aee02b5256b1ca5dacf
Miguel <Miguel Augusto>
2021-06-12 12:22:54 -0300

    create Header component

--- ig-news/src/pages/_document.tsx
+++ ig-news/src/pages/_document.tsx
@@ -12,6 +12,8 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&amp;display=swap" rel="stylesheet" />
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
