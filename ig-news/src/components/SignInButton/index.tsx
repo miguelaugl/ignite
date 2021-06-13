2e4f8f0c7c01dcafb6b2756416168cee5a8bee37
Miguel <miguelaugl@outlook.com>
2021-06-13 09:17:26 -0300

    add authentication with github

--- ig-news/src/components/SignInButton/index.tsx
+++ ig-news/src/components/SignInButton/index.tsx
@@ -1,24 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       className={styles.signInButton}
       type="button"
+      onClick={() => signOut()}
     >
-      <FaGithub color="#04d361" />
-      Miguel Freitas
+      <img src={session.user.image} alt="Github Avatar" />
+      {session.user.name}
       <FiX className={styles.closeIcon} color="#737380" />
     </button>
   ) : (
     <button 
       className={styles.signInButton}
       type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
